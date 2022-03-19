2527cbb1da47c2c7151a892531cfff4bb445e8b4
User <addisumm@gmail.com>
2022-03-19 13:30:18 -0500

    first commit

---
+++ java-streams-start/java-streams-start/src/test/java/com/amigoscode/examples/WorkingWithStreams.java
@@ -0,0 +1,27 @@
+package com.amigoscode.examples;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WorkingWithStreams {
+
+    @Test
+    void steams() {
+        List<String> names = List.of("Amigoscode", "Alex", "Zara");
+        Stream<String> stream =names.stream ();
+        List<Object> collect=stream.limit (2).map (null).sorted (null).dropWhile (null)
+        .collect(Collectors.toList());
+
+        long count=stream
+                        .limit (2)
+                        .map (null)
+                        .sorted (null)
+                        .dropWhile (null)
+                        .count ();
+
+    }
+}
