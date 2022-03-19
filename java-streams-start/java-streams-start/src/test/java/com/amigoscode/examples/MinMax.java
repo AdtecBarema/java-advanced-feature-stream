983623ee6706a3dd9824b576554e05e73517076f
User <addisumm@gmail.com>
2022-03-19 14:15:15 -0500

    min and max

--- java-streams-start/java-streams-start/src/test/java/com/amigoscode/examples/MinMax.java
+++ java-streams-start/java-streams-start/src/test/java/com/amigoscode/examples/MinMax.java
@@ -1,19 +1,20 @@
 package com.amigoscode.examples;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Comparator;
 import java.util.List;
 
 public class MinMax {
-
     @Test
     public void min() {
         List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+        Integer min = numbers.stream ().min (Integer::compareTo).get ();
+        System.out.println (min);
     }
 
     @Test
     public void max() {
         List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+        Integer max = numbers.stream ().max ((o1, o2) -> o1.compareTo (o2)).get ();
+        System.out.println (max);
     }
 }
