c36c94edb6b1d8eff414d50016c137a291cf56d8
User <addisumm@gmail.com>
2022-03-19 23:20:05 -0500

    Data aggregation and statistics

--- java-streams-start/java-streams-start/src/test/java/com/amigoscode/examples/GroupingData.java
+++ java-streams-start/java-streams-start/src/test/java/com/amigoscode/examples/GroupingData.java
@@ -5,21 +5,53 @@ import com.amigoscode.beans.Car;
 import com.amigoscode.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class GroupingData {
 
     @Test
-    public void simpleGrouping() throws Exception {
-        List<Car> cars = MockData.getCars();
+    public void simpleGrouping () throws Exception {
+        List <Car> carList = MockData.getCars ();
+        Map <String, List <Car>> groupByMake = carList
+                .stream ()
+                .collect (Collectors.groupingBy (Car::getMake));
+
+        groupByMake.forEach ((s, cars) -> {
+            System.out.println ("Make " + s);
+            cars.forEach (System.out::println);
+            System.out.println ("--------------------------------------");
+        });
     }
 
     @Test
-    public void groupingAndCounting() throws Exception {
-        List<String> names = List.of(
+    void carGroupByColor () throws IOException {
+        List <Car> carList = MockData.getCars ();
+        Map <String, List <Car>> groupByCarColor = carList.stream ().collect (Collectors.groupingBy (Car::getColor));
+        groupByCarColor.forEach ((color, carListByColor) -> {
+            System.out.println ("Color " + color);
+            carListByColor.forEach (System.out::println);
+            System.out.println ("___________________________________________");
+        });
+    }
+
+    @Test
+    void carGroupModel () throws IOException {
+        List <Car> carList = MockData.getCars ();
+        Map <String, List <Car>> groupByCarModel = carList.stream ()
+                                .collect (Collectors.groupingBy (Car::getModel));
+        groupByCarModel.forEach ((carModel, carListByModel) -> {
+            System.out.println ("Car Model" + carModel);
+            carListByModel.forEach (System.out::println);
+            System.out.println ("_____________________________________________");
+        });
+    }
+
+    @Test
+    public void groupingAndCounting () throws Exception {
+        List <String> names = List.of (
                 "John",
                 "John",
                 "Mariam",
@@ -30,6 +62,7 @@ public class GroupingData {
                 "Alex",
                 "Alex"
         );
+        Map <String, Long> map = names.stream ().collect (Collectors.groupingBy (s -> s, Collectors.counting ()));
+        System.out.println (map);
     }
-
 }
