3f7ba4c713a3664feec83102ffe9f0b1562d4f20
User <addisumm@gmail.com>
2022-03-19 19:14:46 -0500

    Transformations: Map, FlatMap

--- java-streams-start/java-streams-start/src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
+++ java-streams-start/java-streams-start/src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
@@ -15,18 +15,39 @@ import java.util.stream.Collectors;
 public class TransformationsMapAndReduce {
 
     @Test
-    void yourFirstTransformationWithMap() throws IOException {
-        List<Person> people = MockData.getPeople();
+    void yourFirstTransformationWithMap () throws IOException {
+        List <Person> people = MockData.getPeople ();
+
+        Function <Person, PersonDTO> personPersonDTOFunction = person -> {
+            return new PersonDTO (
+                    person.getId (),
+                    person.getFirstName (),
+                    person.getAge ()
+            );
+        };
+
+        List <PersonDTO> personDTOList = people.stream ()
+                .filter (person -> person.getAge () > 94)
+                .filter (person -> person.getGender ().equals ("Male"))
+                .map (PersonDTO::map).collect (Collectors.toList ());
+        personDTOList.forEach (System.out::println);
+        // assertThat(people.size ()).isEqualTo (personDTOList.size ());
     }
 
     @Test
-    void mapToDoubleAndFindAverageCarPrice() throws IOException {
-        List<Car> cars = MockData.getCars();
+    void mapToDoubleAndFindAverageCarPrice () throws IOException {
+        List <Car> cars = MockData.getCars ();
+        double average = cars.stream ().mapToDouble (Car::getPrice).average ().orElse (0);
+        System.out.println (average);
     }
 
     @Test
-    public void reduce() {
+    public void reduce () {
+
         int[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+        int sum = Arrays.stream (integers).reduce (0, Integer::sum);
+        int subtraction = Arrays.stream (integers).reduce (0, (a, b) -> a - b);
+        System.out.println (sum);
+        System.out.println (subtraction);
     }
 }
-
